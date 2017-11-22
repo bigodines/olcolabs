e7f2e5563555539c507f6aab736b409970c3b3c2
bigo <bigodines@gmail.com>
2017-11-21 21:16:50 -0800

    baby steps

--- main.go
+++ main.go
@@ -6,15 +6,24 @@ import (
 	"os"
 
 	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
 	ctx := context.Background()
-	client := github.NewClient(nil)
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+	client := github.NewClient(tc)
 	// list all organizations for user "willnorris"
-	orgs, _, err := client.Organizations.List(ctx, "bigodines", nil)
+	opt := &github.RepositoryListByOrgOptions{Type: "private"}
+	repos, _, err := client.Repositories.ListByOrg(ctx, "gametimesf", opt)
 	if err != nil {
+		println("fuein")
+		println(err.Error())
 		os.Exit(1)
 	}
-	fmt.Printf("%+v", orgs)
+	fmt.Printf("%+v", repos[0])
 }
